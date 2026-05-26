e427f67131c612d6a6e0a475a944cc3f93bbbf6f
Sagar Balamwar <95533401+sagarbalamwar@users.noreply.github.com>
2026-05-26 20:20:31 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+       vector<int>ans;
+       for(int i=0;i<nums.size();i++){
+            int prod=1;
+            for(int j=0;j<nums.size();j++){
+                if(j==i)
+                continue;
+                prod*=nums[j];
+            }
+            ans.push_back(prod);
+       } 
+       return ans; 
+    }
+};
